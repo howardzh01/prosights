75d44189c842bb47aa2aaa9a907348840c4b25e0
kev2010 <cloudwiz24@gmail.com>
2023-12-02 17:23:21 -0500

    bare bones set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-background border-b-0.5 border-gray-400">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-4">
+            <div>
+              <Link
+                href="/"
+                className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
+              >
+                <Image
+                  src="/logo.png"
+                  alt="ProSights logo"
+                  width={32}
+                  height={32}
+                />
+                <span className="font-bold">ProSights</span>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="flex items-center mr-4">
+              <Link
+                href="/about"
+                className="py-5 px-3 text-gray-700 hover:text-gray-900"
+              >
+                About Us
+              </Link>
+            </div>
+            <Link
+              href="/login"
+              className="py-2 px-6 font-bold bg-primary hover:bg-blue-600 text-white rounded-lg transition duration-300"
+            >
+              Login
+            </Link>
+          </div>
+
+          {/* mobile button goes here */}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,44 @@ import React, { Fragment } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 function App() {
-
-
   return (
-    <div className="bg-black">
-      <p className="text-white">Hi howard</p>
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center text-black p-10">
+        <div className="mb-2">
+          <p className="text-primary border border-primary px-6 py-2 rounded-3xl font-bold text-sm">
+            Insight to Action ⚡️
+          </p>
+        </div>
+        <h1 className="text-4xl md:text-5xl font-bold mb-6 text-center">
+          The AI Copilot for{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#009AFF] to-[#B84CEB]">
+            Investors
+          </span>
+          .
+        </h1>
+        <p className="mb-6 text-lg font-light max-w-2xl text-center">
+          Understand any company in 30 seconds by chatting with ProSights. We
+          help you make better decisions by analyzing alternative data.
+        </p>
+        <div className="flex gap-4">
+          <Link
+            href="#"
+            className="bg-white text-blue-500 py-2 px-4 rounded-md hover:bg-gray-100"
+          >
+            Try for Free
+          </Link>
+          <Link
+            href="#"
+            className="py-2 px-4 rounded-md border border-white hover:border-gray-300"
+          >
+            Watch demo
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
