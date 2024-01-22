6e373d42a5c15dfecd0569ea336b5b4196240925
kev2010 <cloudwiz24@gmail.com>
2024-01-22 14:49:32 -0800

    add animation and live click and scroll to sidebar

--- src/components/dashboard/NewSideBar.js
+++ src/components/dashboard/NewSideBar.js
@@ -6,6 +6,13 @@ function NewSideBar({ sections, activeSections }) {
   // Determine if we should extend the bar to cover multiple consecutive active sections
   const isSectionActive = (section) => activeSections[section.title];
 
+  const scrollToSection = (sectionTitle) => {
+    const sectionElement = document.getElementById(sectionTitle);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="bg-customGray-900 h-full w-full px-6 py-4 flex flex-col">
       <Image
@@ -19,13 +26,14 @@ function NewSideBar({ sections, activeSections }) {
         {sections.map((section, index) => (
           <div
             key={index}
-            className={`py-2 text-sm flex items-center ${
+            className={`py-2 text-sm flex items-center cursor-pointer hover:bg-primaryLight hover:rounded-tr-md hover:rounded-br-md hover:bg-opacity-20 transform transition duration-300 ${
               section.level === 1 ? "" : "ml-4"
             } text-customGray-25 border-l-2 border-primaryMedium ${
               isSectionActive(section)
                 ? "text-primaryMedium border-opacity-100"
-                : "border-opacity-0"
-            } pl-2`}
+                : "border-opacity-0 hover:rounded-md"
+            } pl-2 hover:translate-x-2`}
+            onClick={() => scrollToSection(section.title)}
           >
             {section.title}
           </div>
