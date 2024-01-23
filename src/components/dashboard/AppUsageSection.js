df4eec3f0bb113f9f5a5e7dfad6b55d403800df1
kev2010 <cloudwiz24@gmail.com>
2024-01-22 23:40:03 -0800

    Add App Usage Growth vs. Peers section to Dashboard

--- src/components/dashboard/AppUsageSection.js
+++ src/components/dashboard/AppUsageSection.js
@@ -297,6 +297,131 @@ function AppUsageSection({}) {
             </div>
           </div>
         </div>
+        <div id="Traffic Quality" className="content-section mt-8">
+          <p className="text-base font-semibold text-gray-800 mb-3">
+            App Usage Growth vs. Peers
+          </p>
+          <div className="flex flex-row w-full">
+            <div className="inline-block rounded-lg shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white border border-customGray-50 px-6 pt-3 pb-6 w-2/3">
+              <div className="flex flex-row justify-between w-full items-center mb-4">
+                <h2 id="trafficByGeo" className="text-sm font-semibold">
+                  Average MAU Growth
+                </h2>
+                <div className="flex items-center gap-1">
+                  <div className="mr-2 flex items-center">
+                    <button type="button" disabled={true} className="mr-1">
+                      <MinusCircleIcon
+                        className={`w-6 h-6 text-customGray-100
+                }`}
+                      />
+                    </button>
+                    <button type="button" disabled={false}>
+                      <PlusCircleIcon
+                        className={`w-6 h-6 text-customGray-400`}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center mb-2">
+                <Image
+                  src="/assets/globe.svg"
+                  alt="Company Logo"
+                  className="w-4 h-4 object-contain mr-1"
+                  width={128}
+                  height={128}
+                />
+                <p className="text-xs font-normal text-customGray-200 mr-12">
+                  US
+                </p>
+                <Image
+                  src="/assets/graphPictures/visitsGrowthLegend.svg"
+                  alt="Company Logo"
+                  className="w-48 object-contain"
+                  width={1024}
+                  height={1024}
+                />
+              </div>
+              <div className="flex flex-col">
+                <Image
+                  src="/assets/graphPictures/AppMAUGrowthPeersChart.svg"
+                  className="h-72 object-contain"
+                  width={5120}
+                  height={5120}
+                />
+                <div className="overflow-x-auto">
+                  <div className="inline-block w-[90rem] h-auto pb-2">
+                    <Image
+                      src="/assets/graphPictures/AppMAUGrowthPeersTable.svg"
+                      className="h-46 object-contain"
+                      width={5120}
+                      height={5120}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="inline-block rounded-lg shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white border border-customGray-50 px-6 pt-3 pb-6 w-1/3 ml-6">
+              <div className="flex flex-row justify-between w-full items-center mb-4 opacity-0">
+                <h2 id="trafficByGeo" className="text-sm font-semibold">
+                  Total Visits by Channel (%)
+                </h2>
+                <div className="flex items-center gap-1">
+                  <div className="mr-2 flex items-center">
+                    <button type="button" disabled={true} className="mr-1">
+                      <MinusCircleIcon
+                        className={`w-6 h-6 text-customGray-100
+                }`}
+                      />
+                    </button>
+                    <button type="button" disabled={false}>
+                      <PlusCircleIcon
+                        className={`w-6 h-6 text-customGray-400`}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row items-center mb-2 opacity-0">
+                <Image
+                  src="/assets/globe.svg"
+                  alt="Company Logo"
+                  className="w-4 h-4 object-contain mr-1"
+                  width={128}
+                  height={128}
+                />
+                <p className="text-xs font-normal text-customGray-200 mr-12">
+                  US
+                </p>
+                <Image
+                  src="/assets/graphPictures/totalVisitsByChannelLegend.svg"
+                  alt="Company Logo"
+                  className="w-10/12 object-contain"
+                  width={1024}
+                  height={1024}
+                />
+              </div>
+              <div className="flex flex-col">
+                <Image
+                  src="/assets/graphPictures/AppMAUGrowthPeersChartAnnual.svg"
+                  className="h-72 object-contain"
+                  width={5120}
+                  height={5120}
+                />
+                <div className="overflow-x-auto pt-2">
+                  <div className="inline-block w-[30rem] pb-2">
+                    <Image
+                      src="/assets/graphPictures/AppMAUGrowthPeersTableAnnual.svg"
+                      className="h-46 object-contain"
+                      width={5120}
+                      height={5120}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
