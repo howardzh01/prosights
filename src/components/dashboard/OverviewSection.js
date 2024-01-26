49571b003e88f456e9286c247d6a83030a5a7778
Howard Zhong <howardzh@mit.edu>
2024-01-25 21:41:17 -0800

    Fixed overview section line

--- src/components/dashboard/OverviewSection.js
+++ src/components/dashboard/OverviewSection.js
@@ -97,7 +97,7 @@ function OverviewSection({ companyAbout, crunchbaseData, headCountData }) {
           Company Overview
         </p>
       </div>
-      <hr className="border-t border-customGray-50 mt-2" />
+      <hr className="border-none h-px bg-customGray-200" />
       <div className="flex flex-row mt-4 section-indent space-x-24">
         {/* About & Business Model */}
         {companyAbout && companyBusinessModel ? (
