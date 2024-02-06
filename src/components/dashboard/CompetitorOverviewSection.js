16290058ef743dccf6bad2abb0b1d45a0546e778
kev2010 <cloudwiz24@gmail.com>
2024-02-05 17:05:52 -0800

    Add crunchbase logo

--- src/components/dashboard/CompetitorOverviewSection.js
+++ src/components/dashboard/CompetitorOverviewSection.js
@@ -10,11 +10,23 @@ import CompetitorOverviewIcon from "/public/assets/CompetitorOverviewIcon.svg";
 function CompetitorOverviewSection({}) {
   return (
     <div className="flex flex-col w-full">
-      <div className="flex items-center mt-2 mb-3 rounded-md">
-        <CompetitorOverviewIcon className="mx-2 filter invert w-6 h-6" />
-        <p className="text-3xl font-semibold text-gray-800 ">
-          Competitor Overview
-        </p>
+      <div className="flex items-end justify-between mt-2 mb-3 rounded-md">
+        <div className="flex flex-row items-center">
+          <CompetitorOverviewIcon className="mx-2 filter invert w-6 h-6" />
+          <p className="text-3xl font-semibold text-gray-800 ">
+            Competitor Overview
+          </p>
+        </div>
+        <div className="flex flex-row items-center ml-4">
+          <span className="mr-2 italic text-sm text-[#C3C3C3]">Powered by</span>
+          <Image
+            src="/assets/poweredByLogos/crunchbase_logo.svg"
+            alt="coresignal"
+            width="120"
+            height="120"
+            className="h-3 w-auto"
+          />
+        </div>
       </div>
       <hr className="border-none h-px bg-customGray-200" />
       <div className="lex flex-col items-center w-full overflow-hidden section-indent mt-4">
