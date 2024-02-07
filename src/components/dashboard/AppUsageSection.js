b9d74aaafb50de59f7c7a492a6b8ead7bec047d2
kev2010 <cloudwiz24@gmail.com>
2024-02-06 22:44:29 -0800

    add data ai logo

--- src/components/dashboard/AppUsageSection.js
+++ src/components/dashboard/AppUsageSection.js
@@ -18,16 +18,16 @@ function AppUsageSection({}) {
           <AppUsageIcon className="mx-2 filter invert w-6 h-6" />
           <p className="text-3xl font-semibold text-gray-800 ">App Usage</p>
         </div>
-        {/* <div className="flex flex-row items-center ml-4">
+        <div className="flex flex-row items-center ml-4">
           <span className="mr-2 italic text-sm text-[#C3C3C3]">Powered by</span>
           <Image
-            src="/assets/poweredByLogos/semrush_logo.svg"
+            src="/assets/poweredByLogos/data_ai_logo.svg"
             alt="coresignal"
             width="120"
             height="120"
-            className="h-4 w-auto"
+            className="h-6 w-auto"
           />
-        </div> */}
+        </div>
       </div>
       <hr className="border-none h-px bg-customGray-200" />
       <div className="flex flex-col section-indent mt-4">
