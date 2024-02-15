fc398c5d2b84e8c0e7e3b3726472f5c446a257f9
kev2010 <cloudwiz24@gmail.com>
2024-02-14 19:10:40 -0800

    testing endpoints again

--- legacy/components/oldDashboard.js
+++ legacy/components/oldDashboard.js
@@ -2,14 +2,14 @@ import { useState, useEffect, useRef, Fragment, useContext } from "react";
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { useRouter } from "next/router";
-import HeadCountChart from "../../components/charts/HeadCountChart";
-import WebTrafficChart from "../../components/charts/WebTrafficChart";
-import SearchBoxDashboard from "../components/SearchBoxDashboard";
+import HeadCountChart from "../../src/components/charts/HeadCountChart";
+import WebTrafficChart from "../../src/components/charts/WebTrafficChart";
+import SearchBoxDashboard from "./SearchBoxDashboard";
 import WebGeoTrafficChart from "../../components/charts/WebGeoTrafficChart";
 import { Dialog, Transition } from "@headlessui/react";
-import UserProfileButton from "../../components/UserProfileButton";
+import UserProfileButton from "../../src/components/UserProfileButton";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import CompanySummaryView from "../components/CompanySummaryView";
+import CompanySummaryView from "./CompanySummaryView";
 import useSWR from "swr";
 import {
   getGeoTrafficData,
@@ -17,10 +17,10 @@ import {
   getTrafficData,
   getCrunchbaseData,
   getCompanyDescription,
-} from "../../api";
-import ChartModal from "../../components/ChartModal";
+} from "../../src/api";
+import ChartModal from "../../src/components/ChartModal";
 import { createContext } from "react";
-import { RELEVANT_CONTINENTS } from "../../constants";
+import { RELEVANT_CONTINENTS } from "../../src/constants";
 
 // id is the id of the heading, level is the header level e.g. 2 = h2
 const headings = [
