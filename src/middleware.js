e58b8702d3c7b6e060406c24f6ec5e3cb2236db4
kev2010 <cloudwiz24@gmail.com>
2024-02-22 22:18:52 -0800

    another middleware update test

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,10 @@ import { NextResponse } from "next/server";
 
 export default authMiddleware({
   afterAuth({ userId, isPublicRoute }, req) {
+    // Redirect non-signed-in users trying to access non-public URLs to the Clerk sign-in/up page
+    if (!userId && !isPublicRoute) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
     // Check if the user is signed in and trying to access the index page
     if (userId && req.nextUrl.pathname === "/") {
       // Redirect signed-in users trying to access "/" to "/dashboard"
@@ -11,7 +15,7 @@ export default authMiddleware({
     // Allow the request to proceed as normal
     return NextResponse.next();
   },
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/sign-in", "/sign-up"], // Ensure the sign-in page is marked as a public route
 });
 
 export const config = {
