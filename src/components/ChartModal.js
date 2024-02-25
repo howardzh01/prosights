c2553ffe843c4bda4fa98dace3b9a600f9d97ed0
kev2010 <cloudwiz24@gmail.com>
2024-02-25 15:53:41 -0800

    make modal taller

--- src/components/ChartModal.js
+++ src/components/ChartModal.js
@@ -92,7 +92,7 @@ export default function ChartModal({
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
         <div className="fixed inset-0 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -102,7 +102,7 @@ export default function ChartModal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-[80%] sm:p-6 sm:pt-16">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg px-8 bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-[80%]">
                 <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
                   <button
                     type="button"
@@ -113,7 +113,7 @@ export default function ChartModal({
                     <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                   </button>
                 </div>
-                <div className="">{chart}</div>
+                <div className="py-60">{chart}</div>
               </Dialog.Panel>
             </Transition.Child>
           </div>
